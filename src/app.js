4ecb7df5105b3123f423865d1a3770e31668ab8b
Nhan Tran <nhan.tran2@tiki.vn>
2020-11-20 21:06:21 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -26,7 +26,9 @@ app.configure(socketio(require('./socketio')));
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:3000',' http://app.diracnlp.com']
+}));
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
