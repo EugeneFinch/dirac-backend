9ce534227aecd27b26b8a0898a47879030d19df9
Kien <ttkien@trexlog.com>
2020-12-16 16:21:34 +0700

    add calendar event

--- src/services/index.js
+++ src/services/index.js
@@ -8,6 +8,7 @@ const users = require('./users/users.service.js');
 const mailHistory = require('./mail-history/mail-history.service.js');
 const company = require('./company/company.service.js');
 const companyUser = require('./company-user/company-user.service.js');
+const calendarEvent = require('./calendar-event/calendar-event.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(upload);
@@ -20,4 +21,5 @@ module.exports = function (app) {
   app.configure(mailHistory);
   app.configure(company);
   app.configure(companyUser);
+  app.configure(calendarEvent);
 };
