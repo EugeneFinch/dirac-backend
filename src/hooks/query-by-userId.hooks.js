85f1f4a3debf4eab3fd0b0733ddf36aecd50b587
Nhan Tran <nhan.tran2@tiki.vn>
2021-01-21 21:21:51 +0700

    get all recorind in team

--- src/hooks/query-by-userId.hooks.js
+++ src/hooks/query-by-userId.hooks.js
@@ -1,7 +1,25 @@
 const get = require('lodash/get');
 const set = require('lodash/set');
 
-module.exports = function (ctx) {
+module.exports = async function (ctx) {
   const userId = get(ctx,'params.user.id',0);
-  set(ctx,'params.query.user_id',userId);
+  const team = await ctx.app.service('team-user')._find({
+    query:{
+      user_id:userId,
+    },
+  });
+
+  const teamUser = get(team,'data.0');
+  if(!teamUser || teamUser.is_admin != 1) {
+    set(ctx,'params.query.user_id',userId);
+    return; 
+  }
+
+  const allTeamUser = await ctx.app.service('team-user')._find({
+    query:{team_id:teamUser.team_id},
+    paginate:false
+  });
+  const userIds = allTeamUser.map(v=> v.user_id);
+
+  set(ctx,'params.query.user_id',userIds);
 };
