4142c2f70d14e832446f44b4c931f60366c9c6a0
Kirill Mishin <lol33116@gmail.com>
2021-04-01 01:20:02 +0300

    Fix update records

--- src/services/recording/recording.hooks.js
+++ src/services/recording/recording.hooks.js
@@ -5,14 +5,21 @@ const signUrl = require('../../hooks/recordings/sign-url');
 const queryByUserId = require('../../hooks/query-by-userId.hooks');
 const getUserInfo = require('../../hooks/get-user-info.hooks');
 const getCalendarInfo = require('../../hooks/get-meet-info.hook.js')
+const set = require('lodash/set');
 
 module.exports = {
   before: {
     all: [],
-    find: [ authenticate('jwt'),queryByUserId,getUserInfo],
+    find: [authenticate('jwt'), queryByUserId, getUserInfo],
     get: [],
     create: [],
-    update: [],
+    update: [async (ctx) => {
+      const data = ctx.data;
+      const sequelize = ctx.app.get('sequelizeClient');
+      const { recording } = sequelize.models;
+      await recording.update({ deal_status: data.deal_status, account_name: data.account_name },
+        { where: { user_id: data.user_id, account_name: data.old_name ? data.old_name : data.account_name } })
+    }],
     patch: [],
     remove: []
   },
