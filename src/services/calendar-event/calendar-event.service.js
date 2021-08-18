51e4715eebaacc805ff3123d1efd6af6dc4cd4f4
Donald <trung.nguyen@2359media.com>
2021-08-18 17:56:15 +0700

    fix: channel id

--- src/services/calendar-event/calendar-event.service.js
+++ src/services/calendar-event/calendar-event.service.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
     (req, res, next) => {
       if (req.headers) {
         const access_token = get(req, 'headers.x-goog-channel-token');
-        const [env, user_id, timestamp] = get(req, 'headers.x-goog-channel-id', '').split('-');
+        const user_id = get(req, 'headers.x-goog-channel-id', '').split(`${env}-`)[1];
         req.feathers.body = { user_id, access_token };
 
         console.log('dona before /calendar-event: ' + JSON.stringify({ user_id, access_token }));
