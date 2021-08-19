e104a5b45cdb6d49f7109d8b2dc5947df1d20a1a
Donald <trung.nguyen@2359media.com>
2021-08-19 23:17:17 +0700

    fix: remove debuging

--- src/services/meeting-record/auto-agent.js
+++ src/services/meeting-record/auto-agent.js
@@ -52,7 +52,6 @@ const getRecordingName = (roomURL) => {
     args: ['--use-fake-ui-for-media-stream'],
   });
   const page = await browser.newPage();
-  await app.service('cronjob-calendar-event').patch(calendarEventId, { joined: 4 });
 
   try {
     console.log('start', moment().utc().toDate(), roomURL);
@@ -80,7 +79,6 @@ const getRecordingName = (roomURL) => {
     await page.waitForSelector('#password input[type="password"]', { visible: true });
     console.log('Enter email');
     const password = 'dev2021!';
-    await app.service('cronjob-calendar-event').patch(calendarEventId, { joined: 5 });
 
     // Wait for password input
     await page.type('#password input[type="password"]', password);
@@ -88,8 +86,6 @@ const getRecordingName = (roomURL) => {
     console.log('Enter password');
     await page.waitForNavigation();
     console.log('Logged in');
-    await app.service('cronjob-calendar-event').patch(calendarEventId, { joined: 6 });
-
 
     await new Promise((res) => setTimeout(() => res(1), 3000));
     await page.goto(roomURL, { waitUntil: 'load' });
