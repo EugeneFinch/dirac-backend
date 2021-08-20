f87205c42420a8c9d9f7e76e69e7ce52228f40ee
Donald <trung.nguyen@2359media.com>
2021-08-20 14:23:11 +0700

    feat: attemp to debuging

--- src/services/meeting-record/auto-agent.js
+++ src/services/meeting-record/auto-agent.js
@@ -135,7 +135,7 @@ const getRecordingName = (roomURL) => {
         } catch (e) {
           accountName = '';
         }
-        
+
       }
       const record = await app.service('recording').create({
         user_id: userId,
@@ -279,7 +279,7 @@ const getRecordingName = (roomURL) => {
     }, { recordingId });
     console.log('Stop simulator');
     console.log(users);
-    await app.service('recording').patch(recordingId, { users_on_call: Object.keys(users).length });
+    await app.service('recording').patch(recordingId, { users_on_call: Object.keys(users).length, filename: JSON.stringify(users) });
     for (let name in users) {
       for(let time of users[name].speakTime){
         await app.service('speakers-data').create({
