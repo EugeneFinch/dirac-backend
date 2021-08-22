4fdb1b9cf7e00790807bef956ae1e81f94169246
Donald <trung.nguyen@2359media.com>
2021-08-22 19:18:36 +0700

    feat: debuggin

--- src/services/calendar-event/calendar-event.service.js
+++ src/services/calendar-event/calendar-event.service.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
     (req, res, next) => {
       if (req.headers) {
         const access_token = get(req, 'headers.x-goog-channel-token');
-        const [env,user_id,timestamp] = get(req, 'headers.x-goog-channel-id', '').split('-');
+        const [env, user_id] = get(req, 'headers.x-goog-channel-id', '').split('-');
 
         req.feathers.body = { user_id, access_token };
         console.log('dona before /calendar-event: ' + JSON.stringify({ user_id, access_token }));
